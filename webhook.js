bb0e264adaadc9e463f0bc0b4653cf74d8f2f44a
judy112211 <judyeverwyn@gmail.com>
2025-05-31 20:40:13 -0400

    Create webhook.js
    fb-webhook-verifier

---
+++ webhook.js
@@ -0,0 +1,21 @@
+export default function handler(req, res) {
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
+  if (req.method === 'GET') {
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  } else if (req.method === 'POST') {
+    console.log('Received webhook:', req.body);
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(405);
+  }
+}
